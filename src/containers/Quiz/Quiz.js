7f0f27c0b02b85d7381eca19dde69543dae9a302
Damir <deosxz123@gmail.com>
2020-01-04 21:46:45 +0600

    create dynamic

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -4,16 +4,88 @@ import ActvieQuiz from '../../components/ActiveQuiz/ActiveQuiz'
 
 class Quiz extends Component {
   state = {
-    quiz: []
+    activeQuestion: 0,
+    answerState: null,
+    quiz: [
+      {
+        questions: 'What color of the sky?',
+        rightAnswerId: 4,
+        id: 1,
+        answers: [
+          { text: 'Black', id: 1 },
+          { text: 'Green', id: 2 },
+          { text: 'Red', id: 3 },
+          { text: 'Blue', id: 4 }
+        ]
+      },
+      {
+        questions: 'What does the murloc say?',
+        rightAnswerId: 2,
+        id: 2,
+        answers: [
+          { text: 'Row', id: 1 },
+          { text: 'Mrgl mgrl mgrl', id: 2 },
+          { text: 'Ga', id: 3 },
+          { text: 'Awe awe awe', id: 4 }
+        ]
+      }
+    ]
+  }
+
+  onClickAnswerHandler = answerId => {
+    if (this.state.answerState) {
+      const key = Object.keys(this.state.answerState)[0]
+      if (this.state.answerState[key] === 'success') {
+        return
+      }
+    }
+
+
+    const question = this.state.quiz[this.state.activeQuestion]
+
+    if (question.rightAnswerId === answerId) {
+
+      this.setState({
+        answerState: { [answerId]: 'success' }
+      })
+
+      const timeout = window.setTimeout(() => {
+        if (this.isQuizFinished()) {
+          console.log('finished')
+        } else {
+          this.setState({
+            activeQuestion: this.state.activeQuestion + 1,
+            answerState: null
+          })
+        }
+
+        window.clearTimeout(timeout)
+      }, 1000)
+    } else {
+      this.setState({
+        answerState: { [answerId]: 'error' }
+      })
+    }
+  }
+
+  isQuizFinished() {
+    return this.state.quiz.length === this.state.activeQuestion + 1
   }
 
   render() {
     return (
       <div className={classes.Quiz}>
         <div className={classes.QuizWrapper}>
-          <h1>Quiz</h1>
+          <h1>Answer on all questions</h1>
 
-          <ActvieQuiz />
+          <ActvieQuiz
+            answers={this.state.quiz[this.state.activeQuestion].answers}
+            questions={this.state.quiz[this.state.activeQuestion].questions}
+            onAnswerClick={this.onClickAnswerHandler}
+            questionNumber={this.state.activeQuestion + 1}
+            questionsLength={this.state.quiz.length}
+            state={this.state.answerState}
+          />
         </div>
       </div>
     )
