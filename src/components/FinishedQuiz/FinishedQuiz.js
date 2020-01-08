dc1df82661ce918c02ce6ac4db064835012bdc58
Damir <deosxz123@gmail.com>
2020-01-08 16:20:22 +0600

    added validation in Auth

--- src/components/FinishedQuiz/FinishedQuiz.js
+++ src/components/FinishedQuiz/FinishedQuiz.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import classes from './FinishedQuiz.css'
 import Button from '../UI/Button/Button'
+import { Link } from 'react-router-dom'
 
 const FinishedQuiz = props => {
   const rightResults = Object.keys(props.results).reduce((total, key) => {
@@ -34,7 +35,9 @@ const FinishedQuiz = props => {
       <p>Right {rightResults} of {props.quiz.length}</p>
 
       <Button onClick={props.onRetry} type="primary">Again</Button>
-      <Button type="success">To menu of tests</Button>
+      <Link to="/">
+        <Button type="success">To menu of tests</Button>
+      </Link>
     </div>
   )
 }
